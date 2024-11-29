004965690edea217098da5a962a44b764184cd39
Joanna0001 <gkj159@163.com>
2024-11-29 14:53:02 +0800

    jwt令牌

--- src/main/java/com/tlias/controller/LoginController.java
+++ src/main/java/com/tlias/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.tlias.controller;
 import com.tlias.pojo.Emp;
 import com.tlias.pojo.Result;
 import com.tlias.service.EmpService;
+import com.tlias.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -19,6 +23,18 @@ public class LoginController {
     @PostMapping
     public Result login(@RequestBody Emp emp) {
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+
+        // 登录成功，生成令牌，下发令牌
+        if(e != null) {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims); // jwt 包含了当前登录的员工信息
+            return Result.success(jwt);
+        }
+
+        return Result.error("用户名或密码错误");
     }
 }

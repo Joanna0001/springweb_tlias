64061dcd84d9a6b4c8f0a0c983d1ea2255cb6c90
Joanna0001 <gkj159@163.com>
2024-11-29 14:15:12 +0800

    oss存储

--- src/main/java/com/tlias/controller/UploadController.java
+++ src/main/java/com/tlias/controller/UploadController.java
@@ -1,7 +1,9 @@
 package com.tlias.controller;
 
 import com.tlias.pojo.Result;
+import com.tlias.utils.AliOSSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,19 +14,30 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class UploadController {
+    @Autowired
+    private AliOSSUtils aliOSSUtils;
+
     @PostMapping("/upload")
     public Result upload(MultipartFile image) throws Exception {
-        // 获取接收到的文件内容的输入流 getInputStream()
+        /**
+         * 本地存储
+         */
         // 获取原始文件名
-        String originalFilename = image.getOriginalFilename();
+        // String originalFilename = image.getOriginalFilename();
 
         // 构造唯一的文件名（不能重复） -- uuid 通用唯一识别码
-        int index = originalFilename.lastIndexOf(".");
-        String extname = originalFilename.substring(index);
-        String newFileName = UUID.randomUUID().toString() + extname;
+        // int index = originalFilename.lastIndexOf(".");
+        // String extname = originalFilename.substring(index);
+        // String newFileName = UUID.randomUUID().toString() + extname;
 
         // 将文件存储在服务器的磁盘目录中 E:\image
-        image.transferTo(new File("E:\\image\\" + newFileName));
-        return Result.success();
+        // image.transferTo(new File("E:\\image\\" + newFileName));
+        // return Result.success();
+
+        /**
+         * OSS存储
+         */
+        String url = aliOSSUtils.upload(image);
+        return Result.success(url);
     }
 }
